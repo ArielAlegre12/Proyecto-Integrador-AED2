845c6239fcd549ec81c9d042d04b80b741ca0424
ArielAlegre12 <114325019+ArielAlegre12@users.noreply.github.com>
2023-11-21 00:54:29 -0300

    Update GestorDeContactos.c

--- GestorDeContactos.c
+++ GestorDeContactos.c
@@ -1,88 +1,65 @@
 #include<stdio.h>
-#include<stdbool.h>
 #include<string.h>
 #include<stdlib.h>
 #include "listaEnlazada.h"
-
-//PROGRAMA PRINCIPAL
-
-void menuGeneral();
-void menuLista();
-void ingresarDatos();
-
-tListaContacto *listaContacto;
-tInfoContacto infoContacto;
+#include "funcionesArchivo.h" 
 
 
+void menu();
 
-int main(){
-	menuGeneral();
-	
-	return 0;
+int main() {
+    cargarDatosDesdeArchivo();
+    menu();
+    guardarDatosEnArchivo();
+    return 0;
 }
 
-void menuGeneral(){
-	inicializarLista(&listaContacto);	
-	menuLista();
+void menu() {
+    int opcion;
+
+    do {
+        printf("\n----MENU----\n");
+        printf("1 - Agregar Contacto\n");
+        printf("2 - Buscar Contacto\n");
+        printf("3 - Borrar Contacto\n");
+        printf("4 - Guardar Datos\n");
+        printf("5 - Editar un Contacto\n");
+        printf("6 - Mostrar Todos los Contactos\n");
+        printf("7 - Salir\n");
+        scanf("%d", &opcion);
+
+        switch (opcion) {
+            case 1:
+                ingresarDatos();
+                guardarDatosEnArchivo();
+                break;
+            case 2:
+            	buscarContacto();
+                break;
+            case 3:
+                eliminarContacto(listaContacto);
+                break;
+            case 4:
+            	guardarDatosEnArchivo();
+            	break;
+            case 5:
+            	printf("Ingrese el ID del contacto que desea editar: ");
+                int idEditar;
+                scanf("%d", &idEditar);
+                editarContacto(listaContacto, idEditar);
+                guardarDatosEnArchivo();
+                break;
+            case 6:
+            	mostrarContactos();
+            	break;
+            case 7:
+                printf("\nSaliendo del gestor....\n");
+                break;
+            default:
+                printf("\nOpción no válida. Por favor, elija una opción del 1 al 5.\n");
+                break;
+        }
+    } while (opcion != 7);
 }
 
-void ingresarDatos(){
-	printf("\n");
-	printf("ID: ");
-	scanf("%d", &infoContacto.id);
-	printf("Nombre y apellido: ");
-	fflush(stdin);
-	scanf("%[^\n]s", &infoContacto.nombreApellido);
-	printf("Numero de telefono: ");
-	//fflush(stdin);
-	scanf("%s", &infoContacto.numeroTelefono);
-	printf("Sexo(1- varon, 2- mujer): ");
-	scanf("%d", &infoContacto.sexo);
-	printf("Correo electronico: ");
-	fflush(stdin);
-	scanf("%[^\n]s", &infoContacto.correoElectronico);
-	printf("Domicilio: ");
-	fflush(stdin);
-	scanf("%[^\n]s", &infoContacto.domicilio);;
-	printf("Edad: ");
-	scanf("%d", &infoContacto.edad);
-	printf("Fecha de cumple: ");
-	fflush(stdin);
-	scanf("%[^\n]s", &infoContacto.fechaCumple);
-}
 
-void menuLista(){
-	int opcion;
-	int posicion;
-	do{
-		printf("\n");
-		printf("1- Ingresar contacto\n");
-		printf("2- ver lista de contactos\n");
-		printf("3- borrar contacto\n");
-		printf("4- salir\n");
-		printf("Opcion: ");
-		scanf("%d", &opcion);
-		
-		switch(opcion){
-			case 1:
-				ingresarDatos();
-				insertarElemento(&listaContacto, &infoContacto);
-				break;
-			case 2:
-				printf("\nLISTA DE CONTACTOS\n");
-				visualizarContactos(listaContacto, &infoContacto);
-				break;
-			case 3:
-				printf("\nQue elemento desea eliminar: ");
-				scanf("%d", posicion);
-				eliminarEnPosK(posicion, &listaContacto, &infoContacto);
-				break;
-			case 4:
-				printf("\nSaliendo del menu\n");
-				break;
-			default:
-				printf("\nLa opcion que ingresaste no es correcta, ingresa otra por favor\n");
-				break;
-		}
-	}while(opcion!=4);
-}
